51011c8cc9e64230e5f4b5294a6c0ef7ecadf341
netrunner <netrunnerr25@gmail.com>
2026-03-12 14:13:49 +0900

    feat: main.py 라우터 추가

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
 from app.db.base import initialize_tortoise
+from app.routers import diary_api
 
 app = FastAPI()
 
+app.include_router(diary_api.router)
 
 initialize_tortoise(app)
